9a765bfac4326434674f8ba163718a6b302018d4
Wectornanime <81574689+Wectornanime@users.noreply.github.com>
2024-07-04 23:41:04 -0300

    added settings

--- src/routes/drawerRouter.js
+++ src/routes/drawerRouter.js
@@ -1,6 +1,7 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import HomeScreen from "../screens/HomeScreen";
+import Settings from "../screens/Settings";
 
 const Drawer = createDrawerNavigator();
 
@@ -8,6 +9,7 @@ export default function DrawerRouter() {
     return (
         <Drawer.Navigator>
             <Drawer.Screen name="Home" component={HomeScreen} />
+            <Drawer.Screen name="Settings" component={Settings} />
         </Drawer.Navigator>
     )
 }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,44 +1,78 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 import { Ionicons } from '@expo/vector-icons';
+import axios from 'axios';
 
 export default function HomeScreen() {
-    const [locations, setLocations] = useState([
-        {
-            "id": 1,
-            "title": "location 1",
-            "coords": {
-                "latitude": -8.0656897,
-                "longitude": -34.8737569,
-                "icon": "home"
-            }
-        },
-        {
-            "id": 2,
-            "title": "location 2",
-            "coords": {
-                "latitude": -8.065380,
-                "longitude": -34.873414,
-                "icon": "home"
-            }
-        },
-        {
-            "id": 3,
-            "title": "location 3",
-            "coords": {
-                "latitude": -8.065485,
-                "longitude": -34.877424,
-                "icon": "home"
-            }
+    const [region, setRegion] = useState(null);
+    const [userLocation, setUserLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+    const [riskLocation, setRiskLocation] = useState([]);
+
+    const findRisks = async () => {
+        try {
+            const response = await axios.get('http://dados.recife.pe.gov.br/api/3/action/datastore_search?resource_id=5eaed1e8-aa7f-48d7-9512-638f80874870');
+            let data = [];
+            response.data.result.records.map(rec => {
+                if (rec.latitude && rec.longitude) {
+                    data.push({
+                        "id": rec._id,
+                        "title": rec.solicitacao_endereco,
+                        "coords": {
+                            "latitude": rec.latitude,
+                            "longitude": rec.longitude
+                        },
+                        "icon": "alert-circle"
+                    })
+                }
+            })
+
+            setRiskLocation(data)
+        } catch (error) {
+            console.error('Erro ao buscar dados:', error);
+            throw error; // Trate o erro conforme necessário
         }
-    ]);
+    };
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('A permissão para acessar o local foi negada!');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+
+            setUserLocation(location);
+            setRegion({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.02,
+                longitudeDelta: 0.02,
+            });
+        })();
+
+        findRisks();
+    }, []);
 
     return (
-        <View style={styles.container}>
-            <MapView style={styles.map}>
-                {locations.map(location => (
+        <View style={styles.container} region={region}>
+            <MapView style={styles.map} region={region}>
+
+                {userLocation && (
+                    <Marker
+                        coordinate={{
+                            latitude: userLocation.coords.latitude,
+                            longitude: userLocation.coords.longitude,
+                        }}
+                        title="Você está aqui"
+                    />
+                )}
+
+                {riskLocation.map(location => (
                     <Marker key={location.id}
                         coordinate={{
                             latitude: location.coords.latitude,
@@ -46,9 +80,10 @@ export default function HomeScreen() {
                         }}
                         title={location.title}
                     >
-                        <Ionicons name={location.namicon} size={size = 30} />
+                        <Ionicons name={location.icon} size={size = 30} />
                     </Marker>
                 ))}
+
             </MapView>
         </View>
     )
